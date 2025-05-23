de6037ae63c5f7c81c8be80df3d4d5931e9a20d7
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-05-23 12:17:16 +0300

    Class Refactor

--- E-Commearce.web/Program.cs
+++ E-Commearce.web/Program.cs
@@ -1,6 +1,7 @@
 
 using DomainLayer.Contracts;
 using E_Commearce.web.CustomeMiddleWare;
+using E_Commearce.web.Extensions;
 using E_Commearce.web.Factories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,12 +13,13 @@ using Service;
 using Service.MappingProfiles;
 using ServiceAbstraction;
 using shared.ErrorModels;
+using System.Threading.Tasks;
 
 namespace E_Commearce.web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -29,21 +31,14 @@ namespace E_Commearce.web
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<StoreDbContext>(Options =>
-            {
-                Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddAutoMapper(typeof(ProductProfile).Assembly);
-            builder.Services.AddScoped<IServiceManager, serviceManager>();
-            builder.Services.Configure<ApiBehaviorOptions>((Options) =>
-            {
-                Options.InvalidModelStateResponseFactory = ApiResponseFactory.GenerateApiErrorReponse;
-               
-            });
+           
+
+
+            builder.Services.AddSwaggerService();
+            builder.Services.AddInfrastructureService(builder.Configuration);
+            builder.Services.AddApplicationService();
+            builder.Services.AddWebApplicationServices(); 
+           
             
             #endregion
 
@@ -53,10 +48,7 @@ namespace E_Commearce.web
 
             #region DataSeeding
 
-            using var scope = app.Services.CreateScope();
-
-            var ObjectOfDataSeeding = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
-            ObjectOfDataSeeding.DataSeedAsync();
+            await app.SeedDataBaseAsync();
             #endregion
 
 
@@ -75,9 +67,10 @@ namespace E_Commearce.web
 
             //});
 
-            
 
-            app.UseMiddleware<CustomExceptionhandlerMiddleWare>();
+
+
+            app.UseCustomeMiddleWare();
             
             if (app.Environment.IsDevelopment())
             {
---
+++ Infrastructure/Peristance/InfrastructureServiceRegisteration.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peristance
+{
+    public static class InfrastructureServiceRegisteration
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection Services,IConfiguration Configuration)
+        {
+            Services.AddDbContext<StoreDbContext>(Options =>
+            {
+                Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            Services.AddScoped<IDataSeeding, DataSeeding>();
+            Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return Services;
+        }
+    }
+}
