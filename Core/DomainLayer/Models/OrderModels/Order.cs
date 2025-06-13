b0f1ca9bc0e79e8eb28aff94a0f92091b19d7062
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-06-14 02:42:03 +0300

    Order Module

---
+++ Core/DomainLayer/Models/OrderModels/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModels
+{
+    public class Order:BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public OrderAddress Address { get; set; } = default!;
+
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+
+        public int DeliveryMethodId { get; set; }//FK
+
+        public OrderStatus OrderStatus { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; } = [];
+
+        public decimal Subtotal { get; set; }
+
+        //[NotMapped]
+        //public decimal Total { get=> SubTotal }
+        public decimal GetTotal() => Subtotal + DeliveryMethod.Price;
+    }
+
+}
