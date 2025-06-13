bb8c49d8f9080e796a6854529803ca71331eb7d4
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-06-14 00:28:39 +0300

    Controller

--- Infrastructure/Presentation/Controller/AuthentactionController.cs
+++ Infrastructure/Presentation/Controller/AuthentactionController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
 using shared.DataTransferObjects.IdentityDTO_S;
+using System.Security.Claims;
 
 namespace E_Commearce.web.Controllers
 {
@@ -17,6 +19,8 @@ namespace E_Commearce.web.Controllers
 
 
         //Register
+
+
         [HttpPost("Register")]
         public async Task<ActionResult<UserDTO>> Register(RegisterDto registerDto)
         {
@@ -24,6 +28,43 @@ namespace E_Commearce.web.Controllers
             return Ok(User);
         }
 
+        //check Email
+        [HttpGet("CheckEmail")]//Get BaseUrl/api/Authentication/CheckEmail
 
+        public async Task<ActionResult<bool>> CheckEmail(string email)
+        {
+            var Result =await _serviceManager.Authentaction.CheckEmailAsync(email);
+            return Ok(Result);
+        }
+
+        //Get Current User
+        [Authorize]//inside token we send email
+        [HttpGet("CurrentUser")] //Get BaseUrl/api/Authentaction/checkEmail
+        public async Task<ActionResult<UserDTO>> GetCurrentUser()
+        {
+           var email=User.FindFirstValue(ClaimTypes.Email);
+            var AppUer =await _serviceManager.Authentaction.GetCurrentUserAsync(email!);
+            return Ok(AppUer);
+        }
+
+        //Get Current User Address
+        [Authorize]
+        [HttpGet("Address")]
+        public async Task<ActionResult<AddressDto>> GetCurrentUserAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var Address =await _serviceManager.Authentaction.GetCurrentAddressAsync(email!);
+            return Ok(Address);
+        }
+
+        //Update Current User Address
+        [Authorize]
+        [HttpPut("Address")]
+        public async Task<ActionResult<AddressDto>>UpdateCurrentUserAddress(AddressDto addressDto)
+        {
+            var email=User.FindFirstValue(ClaimTypes.Email);
+            var UpdatedAddress =await _serviceManager.Authentaction.UpdateCurrentUserAddress(email!, addressDto);
+            return Ok(UpdatedAddress);
+        }
     }
 }
