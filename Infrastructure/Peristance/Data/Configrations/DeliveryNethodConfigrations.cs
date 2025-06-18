afa27751f20ad57e9382bd46fda6d04dbb80c686
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-06-18 20:23:42 +0300

    Configrations

---
+++ Infrastructure/Peristance/Data/Configrations/DeliveryNethodConfigrations.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Peristance.Data.Configrations
+{
+    //
+    internal class DeliveryNethodConfigrations : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.ToTable("DeliveryMethods");
+            builder.Property(D=>D.Price)
+                .HasColumnType("decimal(8,2)");
+
+            builder.Property(D => D.ShortName)
+                .HasColumnType("varchar")
+                .HasMaxLength(50);
+
+            builder.Property(D => D.Description)
+                .HasColumnType("varchar")
+                .HasMaxLength (100);
+
+            builder.Property(D => D.DeliveryItem)
+                .HasColumnType("varchar")
+                .HasMaxLength(50);
+
+        }
+    }
+}
---
+++ Infrastructure/Peristance/Data/Configrations/OrderConfigrations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peristance.Data.Configrations
+{
+    internal class OrderConfigrations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+
+            builder.Property(O => O.Subtotal)
+                .HasColumnType("decimal(8,2)");
+
+            builder.HasMany(O => O.Items)
+                 .WithOne();
+
+
+            builder.HasOne(O => O.DeliveryMethod)
+                    .WithMany()
+                    .HasForeignKey(O => O.DeliveryMethodId);
+
+            builder.OwnsOne(O => O.Address);
+            //to be mapped as column in table order
+        }
+    }
+}
