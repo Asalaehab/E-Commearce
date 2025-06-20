647fd9b1f2ffa4aed54d8b4d77a9d15357507ca2
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-06-20 14:06:48 +0300

    Mapping

--- Core/Service/MappingProfiles/OrderProfile.cs
+++ Core/Service/MappingProfiles/OrderProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using DomainLayer.Models.OrderModels;
 using shared.DataTransferObjects.IdentityDTO_S;
+using shared.DataTransferObjects.OrderDTO_S;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,16 @@ namespace Service.MappingProfiles
         public OrderProfile()
         {
             CreateMap<AddressDto, AddressDto>();
+
+            CreateMap<Order, OrderToReturn>()
+                .ForMember(D => D.DeliveryMethod, O => O.MapFrom(S => S.DeliveryMethod.ShortName));
+
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(D => D.ProductName, O => O.MapFrom(S => S.Product.ProductName))
+                .ForMember(D => D.PictureUrl, O => O.MapFrom<OrderItemPictureUrlResolver>());
+
+
+
         }
     }
 }
