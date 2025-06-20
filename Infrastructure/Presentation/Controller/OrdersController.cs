010b3952d26eb4de382d05f6d38fa4238a0b0bf1
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-06-20 17:08:46 +0300

    Controller

---
+++ Infrastructure/Presentation/Controller/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using E_Commearce.web.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using shared.DataTransferObjects.OrderDTO_S;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class OrdersController(IServiceManager _serviceManager) : APIBaseController
+    {
+        //Create Order
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<OrderToReturn>>createOrder(OrderDto orderDto)
+        {
+            //var Email=User.FindFirstValue(ClaimTypes.Email);//get email from token
+
+            var Order =await _serviceManager.OrderService.CreateOrder(orderDto, GetEmailFromToken());
+
+            return Ok(Order);
+        }
+
+    }
+}
